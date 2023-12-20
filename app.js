10c6f0ac156e138843f63095c7ecce8b200faa4f
Heber <116190313+hpagon@users.noreply.github.com>
2023-12-20 14:46:27 -0700

    added function to check passwords validity

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
+// password inputs
 let password = document.querySelector("#password");
 let passwordConfirm = document.querySelector("#password_confirm");
+// toggle buttons
 let passwordShow = document.querySelector("#passwordShow");
 let confirmPasswordShow = document.querySelector("#confirmPasswordShow");
+//confirm password input error message
+let confirmPasswordError = passwordConfirm.nextElementSibling;
 
-console.log(password);
-console.log(passwordConfirm);
-console.log(passwordShow);
-console.log(confirmPasswordShow);
-
-passwordShow.addEventListener("click", (e) => {
-    password.type = password.type === 'password' ? 'text':'password';
+// toggles the password input type between password and text
+function passwordToggle(e) {
+    let passwordInput = e.target.nextElementSibling;
+    passwordInput.type = passwordInput.type === 'password' ? 'text':'password';
     e.target.textContent = e.target.textContent === 'Show' ? 'Hide':'Show';
+}
+
+function checkPasswordValidity() {
+    console.log("here?")
+    if(passwordConfirm.value === "") {
+        passwordConfirm.setCustomValidity("Invalid");
+        confirmPasswordError.textContent = "Passwords Must Match.";
+    } else if(password.value === passwordConfirm.value && password.checkValidity()) {
+        console.log("custom validity?")
+        passwordConfirm.setCustomValidity("");
+    } else if(password.value === passwordConfirm.value) {
+        passwordConfirm.setCustomValidity("Invalid");
+        confirmPasswordError.textContent = "Password must follow requirements.";
+    } else {
+        passwordConfirm.setCustomValidity("Invalid")
+        confirmPasswordError.textContent = "Passwords Must Match.";
+    }    
+}
+
+
+// events listeners
+passwordShow.addEventListener('click', (e) => {
+    passwordToggle(e);
 })
+confirmPasswordShow.addEventListener('click', (e) => {
+    passwordToggle(e);
+})
+
+// checks validity of confirm password field
+passwordConfirm.addEventListener('keyup', checkPasswordValidity);
