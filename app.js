55defd2ad89dfb8eb1987cf7aaa26a813dd07c52
Heber <116190313+hpagon@users.noreply.github.com>
2023-12-19 22:46:06 -0700

    added functionality to show buttons

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 let password = document.querySelector("#password");
 let passwordConfirm = document.querySelector("#password_confirm");
+let passwordShow = document.querySelector("#passwordShow");
+let confirmPasswordShow = document.querySelector("#confirmPasswordShow");
 
 console.log(password);
 console.log(passwordConfirm);
+console.log(passwordShow);
+console.log(confirmPasswordShow);
 
+passwordShow.addEventListener("click", (e) => {
+    password.type = password.type === 'password' ? 'text':'password';
+    e.target.textContent = e.target.textContent === 'Show' ? 'Hide':'Show';
+})
